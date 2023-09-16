9f50419a816f9723bead8021298e856f0356d414
swn233 <swndaze@gmail.com>
2023-09-16 19:51:30 +0800

    init

---
+++ my-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Bean;
+
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    }
+}
