053af92a392e14c47129d7761394904102d9e768
swn233 <swndaze@gmail.com>
2023-09-23 15:01:35 +0800

    springsecurity配置成功

---
+++ my-project-backend/src/main/java/com/example/entity/RestBean.java
@@ -0,0 +1,24 @@
+package com.example.entity;
+
+import com.alibaba.fastjson2.*;
+
+public record RestBean<T>(int code, T data, String message) {
+
+
+    public static <T> RestBean<T> success(T data) {
+        return new RestBean<>(200, data, "Success");
+    }
+
+    public static <T> RestBean<T> success() {
+        return success(null);
+    }
+
+    public static <T> RestBean<T> failure(int code,String message) {
+        return new RestBean<>(code ,null,message);
+    }
+    public String asJsonString() {
+     return  JSONObject.toJSONString(this,JSONWriter.Feature.WriteNulls);
+    }
+
+
+}
